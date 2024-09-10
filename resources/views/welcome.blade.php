4407f650501921d892d84f4b3a696e33f988c91d
xfshahrukh44 <ssidduit@gmail.com>
2024-09-11 01:57:52 +0500

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+@section('content')
+
+<!-- ============================================================== -->
+<!-- BODY START HERE -->
+<!-- ============================================================== -->
+
+
+@endsection
+@section('css')
+<style>
+
+</style>
+@endsection
+
+@section('js')
+<script type="text/javascript"></script>
+@endsection
